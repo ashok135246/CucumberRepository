d9a13cf83c4417058e577951e97f3dc3245accdb
ashok135246 <78471082+ashok135246@users.noreply.github.com>
2021-02-03 20:52:00 +0530

    Update TestRunner.java

--- CucumberMaven/src/test/java/Runner/TestRunner.java
+++ CucumberMaven/src/test/java/Runner/TestRunner.java
@@ -7,8 +7,8 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features= {"C:\\Users\\IBMADMIN\\Documents\\eclipse\\eclipse-workspace\\CucumberMaven\\src\\test\\resources\\Login\\Login.feature"},
-glue= {"stepdefinition"},//This is to run step files
+@CucumberOptions(features= {"src\\test\\resources\\Login\\Login.feature"},
+glue= {"StepDefinition"},//This is to run step files
 monochrome=true, //to make console output in a better format
 dryRun=false,//to check all methods have been implemented properly
 strict=true)//to check if i missed any steps in step definition
