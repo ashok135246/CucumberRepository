48ff40b701ec7d8a7c4aaef79ea70d497f188db8
ashok135246 <78471082+ashok135246@users.noreply.github.com>
2021-02-03 20:53:06 +0530

    Update Steps.java

--- CucumberMaven/src/test/java/StepDefinition/Steps.java
+++ CucumberMaven/src/test/java/StepDefinition/Steps.java
@@ -10,51 +10,46 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Steps {
-	WebDriver driver;
+	WebDriver driver;	
 	
-	
-	@Given("^Login application present$")
+	@Given("Login application present")
 	public void login_application_present() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\IBMADMIN\\Documents\\Python training\\driver\\chromedriver.exe");
 		driver=new ChromeDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 	    
 	}
-
-
-	@When("^Enter valid credentials$")
+	
+	@When("Enter valid credentials")
 	public void enter_valid_credentials() throws InterruptedException {
-		 driver.findElement(By.id("txtUsername")).sendKeys("Admin");
-		 driver.findElement(By.id("txtPassword")).sendKeys("admin123");
-		 driver.findElement(By.id("btnLogin")).click();
-		 Thread.sleep(3000);
-	    
+		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("admin123");
+	    driver.findElement(By.xpath("//input[@id='btnLogin']")).click();
+	    Thread.sleep(3000);
 	}
-	@Then("^Login successful$")
+
+	@Then("Login successful")
 	public void login_successful() {
 		String expected=driver.findElement(By.id("welcome")).getText();
-		String Actual="Welcome Harshith";
+		String Actual="Welcome Paul";
 		Assert.assertEquals(expected,Actual);
 		driver.findElement(By.id("welcome")).click();
 		driver.findElement(By.xpath("//*[@id='welcome-menu']/ul/li[2]/a")).click();
-	    
-	}
-
-	@When("^Enter invalid credentials$")
-	public void enter_invalid_credentials() throws InterruptedException {
-		driver.findElement(By.id("txtUsername")).sendKeys("Admin123");
-		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
-		driver.findElement(By.id("btnLogin")).click();
-		Thread.sleep(3000);
-	    
 	}
 	
-	@Then("^Login unsuccessfull$")
+	@When("Enter invalid credentials")
+	public void enter_invalid_credentials() throws InterruptedException {
+		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("admin123");
+	    driver.findElement(By.xpath("//input[@id='btnLogin']")).click();
+		Thread.sleep(3000);
+	}
+	
+	@Then("Login unsuccessfull")
 	public void login_unsuccessfull() {
-	    String error=driver.findElement(By.xpath("//*[@id='spanMessage']")).getText();
+		String error=driver.findElement(By.xpath("//*[@id='spanMessage']")).getText();
 	    String Actualerror="Invalid credentials"; 
 	    Assert.assertEquals(error, Actualerror);
 	}
-
-
+	
 }
